7a05388d958ef962e0a7d6b43e8041b51d6b9cea
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2025-09-07 16:05:55 +0800

    feat: 添加接口说明页面

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const path = require('path');
 
 const app = express();
 const PORT = 3986;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, './')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
+});
 
 const router = require("./router.js");
 app.use(router);
